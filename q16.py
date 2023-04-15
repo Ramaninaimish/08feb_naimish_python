eff68512059ad2d13f87a09886283509306eb606
Ramaninaimish <124854876+Ramaninaimish@users.noreply.github.com>
2023-04-15 10:04:59 +0530

    Delete q16.py

--- q16.py
+++
@@ -1,7 +0,0 @@
-list = ['a','x',[1,3,5,6,'zalak']]
-for i in list:
-    if len(i) > 1:
-        print("sublist is present in list")
-        break
-else:
-    print("sublist is not present in list")
