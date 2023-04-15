05c9fb9bbbcc17bcca6ddfc6fe5c101c9cefa6aa
Ramaninaimish <124854876+Ramaninaimish@users.noreply.github.com>
2023-04-15 10:15:06 +0530

    Delete q61.py

--- q61.py
+++
@@ -1,6 +0,0 @@
-# Write a Python program to calculate the area of a parallelogram
-
-base=float(input("length of base :"))
-height=float(input("measurment of height"))
-area=base*height
-print(area)
